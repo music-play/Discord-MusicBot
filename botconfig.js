3c6956d1b5e253b09726e4f23443826c247efae7
music-play <100921077+music-play@users.noreply.github.com>
2022-03-04 16:07:42 +0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,15 +4,15 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "OTQ5MjI5OTQ2MjQzNTE4NDk0.YiHVRw.MDTITJVdsT_r7KF0qFuE0HJ0T5o", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "949229946243518494", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "yE7rqcEsI1OOquGn2CPZUzqwJOUrab9S", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Pikachu is EPIC", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
@@ -30,15 +30,15 @@ module.exports = {
   // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "losingtime.dpaste.org", //- The host name or IP of the lavalink server.
-    port: 2124, // The port that lavalink is listening to. This must be a number!
-    pass: "SleepingOnTrains", //- The password of the lavalink server.
-    secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
+    host: "connect.freelavalink.ga", //- The host name or IP of the lavalink server.
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "www.freelavalink.ga", //- The password of the lavalink server.
+    secure: true, // Set this to true if the lavalink uses SSL. if not set it to false.
   },
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "3a08b047b19d44aaa1a16fb748832e10", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "c1a3425cb1564af196067b2c40ed58e6", //Spotify Client Secret
   },
 };
